a03570fc965791ce0f8c07645616069a141fa38b
Vincent Murphy <vdm@vdm.ie>
2021-10-17 20:14:08 +0100

    remove dup

--- custom.el
+++ custom.el
@@ -3,7 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(visible-bell t))
+ '(desktop-save-mode t)
+ '(package-selected-packages '(markdown-mode))
+ '(savehist-mode t)
+ '(tab-bar-history-mode t)
+ '(visible-bell t)
+ '(vterm-max-scrollback 100000))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
